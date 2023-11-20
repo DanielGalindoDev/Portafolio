8936a56c2a3acf499245c7aa39f6352f3503b5a5
Daniel Galindo <mseragonf@gmail.com>
2023-11-20 17:01:04 -0600

    Changes to be committed: 	modified:   html/Educacion.html 	modified:   html/Habilidades.html 	modified:   html/Proyectos.html 	modified:   html/Sobre_mi.html 	modified:   js/logica.js

---
+++ js/logica.js
@@ -0,0 +1,11 @@
+document.getElementById('imagen1').addEventListener('click', function() {
+
+});
+
+document.getElementById('imagen2').addEventListener('click', function() {
+
+});
+
+document.getElementById('imagen3').addEventListener('click', function() {
+
+});
